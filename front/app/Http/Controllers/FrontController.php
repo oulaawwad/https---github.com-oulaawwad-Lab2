769d8678d33e1b112b78e980aef3848c469e815e
oulaawwad <olaawwad8@gmail.com>
2020-12-10 14:39:25 +0200

    getbook cache

--- front/app/Http/Controllers/FrontController.php
+++ front/app/Http/Controllers/FrontController.php
@@ -20,23 +20,35 @@ class FrontController extends Controller{
 			$book=Cache::get($type);
 			return response()->json(json_decode($book));
 		}
+		else{
 
 		$url = '';
 		$url = 'http://192.168.1.23:8000/query/booktype/' . $type;//// my ip address
 		
 		$page = file_get_contents($url); 
+		Cache::put($type , $page ,300);
 		return response()->json(json_decode($page));
+		}
 	}
 	/////////////////////////////////////////////////////////////////////////////////////////////
 
 
 	public function getBook($id){
+		if (Cache::has($id))
+		{	
+
+			$book=Cache::get($id);
+			return response()->json(json_decode($book));
+		}
+		else{
 	
 		$url = '';
         $url = 'http://192.168.1.23:8000/query/bookid/' . $id;
 		
 		$page = file_get_contents($url); 
+		Cache::put($id , $page ,300);
 		return response()->json(json_decode($page));
+		}
 	}
 
 
